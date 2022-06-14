ecc45f012f02e3f37c8e67d8eca4dd96dc0c9e74
william <101874444+snowman12320@users.noreply.github.com>
2022-06-14 12:37:56 +0800

    JQ實作

---
+++ JQuery/all.js
@@ -0,0 +1,21 @@
+
+
+// // 初始化行為
+// $(function(){
+//     console.log('插旗');
+// })
+
+// //
+
+// $('#hideBtn').on('click',function(){
+//     //下面是 要操作的元素，上面是事件監聽哪一種、要做什麼事
+//     $('.block').toggle();
+// });
+
+// //
+
+// $(function () {
+//     $('button');
+//     console.log($('button'));
+// });
+
